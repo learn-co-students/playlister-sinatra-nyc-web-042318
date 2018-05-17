233c72116c50bc222bd320621fb7c448b8ec1c68
blevm <mjblevins7@gmail.com>
2018-05-17 11:33:02 -0400

    built model relationships and methods

---
+++ app/models/song.rb
@@ -0,0 +1,17 @@
+class Song < ActiveRecord::Base
+  belongs_to :artist
+
+  has_many :song_genres
+  has_many :genres, through: :song_genres
+
+  def slug
+    self.name.downcase.gsub(/[ .!,]/, "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find do |song|
+      slug == song.slug
+    end
+  end
+
+end
