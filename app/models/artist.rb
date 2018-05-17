60032ce44404b241ffa8c944b95760c2e9626238
ensallee <ensallee@gmail.com>
2018-05-17 13:06:10 -0400

    finished models and began building songs controller

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,14 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+  has_many :genres, through: :songs
+
+  def slug
+    self.name.gsub(/[^0-9A-Za-z]/, '-').downcase
+  end
+
+  def self.find_by_slug(slug)
+    Artist.all.find do |artist|
+      artist.slug == slug
+    end
+  end
 end
