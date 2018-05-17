04f5c590b2e50bdb2f38f52542b15315ec61fd5b
ensallee <ensallee@gmail.com>
2018-05-17 14:18:16 -0400

    created genres/show and started songs/new

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,10 +5,21 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    @artists = Artist.all
+    @genres = Genre.all
+    erb :'songs/new'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
 
     erb :'songs/show'
+  end
+
+  post '/songs' do
+
+    redirect "/songs/#{@song.slug}"
   end
 
 end
