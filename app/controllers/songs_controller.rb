6372e78121d2429ab2886d0066864d5580d4e6d4
ensallee <ensallee@gmail.com>
2018-05-17 17:20:36 -0400

    solved

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,7 @@
-
 class SongsController < ApplicationController
+  require 'rack-flash'
+  use Rack::Flash
+  enable :sessions
 
   get '/songs' do
     @songs = Song.all
@@ -34,5 +36,32 @@ class SongsController < ApplicationController
     flash[:message] = "Successfully created song."
     redirect "/songs/#{@song.slug}"
   end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    @genres = Genre.all
+    @artists = Artist.all
+
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+
+    if !params["artist"]["name"].empty?
+      if Artist.all.find_by(name: params["artist"]["name"])
+        @song.artist = Artist.all.find_by(name: params["artist"]["name"])
+      else
+        @song.artist = Artist.create(params["artist"])
+      end
+    end
+    params[:song][:genre_ids].each do |genre_id|
+      @song.genres << Genre.find(genre_id)
+    end
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect "/songs/#{@song.slug}"
+    end
+  # end
 
 end
