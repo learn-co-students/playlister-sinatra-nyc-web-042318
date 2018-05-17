c75f098779f220d3bb6a0f261352d6437ad43377
blevm <mjblevins7@gmail.com>
2018-05-17 14:41:01 -0400

    made some progress!

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,9 @@
 class SongsController < ApplicationController
 
+  require 'rack-flash'
+  use Rack::Flash
+  enable :sessions
+
   get '/songs' do
     @songs = Song.all
 
@@ -8,27 +12,9 @@ class SongsController < ApplicationController
 
   get '/songs/new' do
     @artists = Artist.all
+    @genres = Genre.all
 
     erb :"songs/new"
-  end
-
-  post '/songs' do
-    # binding.pry
-    @song = Song.create(params[:song])
-
-    if !params[:artist][:name].empty?
-      @song.artist = Artist.create(params[:artist])
-    end
-    @song.save
-
-    redirect "/songs/#{@song.slug}"
-  end
-
-  get "/songs/:slug/edit" do
-    @song = Song.find_by_slug(params["slug"])
-    @artists = Artist.all
-
-    erb :"songs/edit"
   end
 
   get "/songs/:slug" do
@@ -36,6 +22,37 @@ class SongsController < ApplicationController
 
     erb :"songs/show"
   end
+
+  post '/songs' do
+    #binding.pry
+    @song = Song.create(params[:song])
+
+    if !params[:genre][:name].empty?
+      @song.genres << Genre.create(params[:genre])
+    end
+
+    if !params[:artist][:name].empty?
+      @song.artist = Artist.create(params[:artist])
+    end
+  #  binding.pry
+
+
+    @song.save
+    flash[:message] = "Successfully created song."
+
+
+    redirect "/songs/#{@song.slug}"
+  end
+
+  get "/songs/:slug/edit" do
+    @song = Song.find_by_slug(params["slug"])
+    @artists = Artist.all
+    @genres = Genre.all
+
+    erb :"songs/edit"
+  end
+
+
 
 
 
--- config.ru
+++ config.ru
@@ -4,9 +4,6 @@ require './config/environment'
 #   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 # end
 
-enable :sessions
-
-
 
 use Rack::MethodOverride
 
